da8e7d98d147374a57344d9945d0b506e5743747
Your Name <you@example.com>
2023-04-18 13:31:50 +0530

    2

--- vanill.java
+++ vanill.java
@@ -7,11 +7,15 @@ class x{
          int x2 = 0;
          int res = x1/x2;
          System.out.println("Result came after division is: " + res);  
-
-    }
-    catch(ArithmeticException e){
+}
+    
+    catch(Throwable e){
          System.out.println("Failed to write to file due to the following error: " + e );
-
+         e.printStackTrace();
     }
+
+System.out.println("111111111111111111111111111");
+
   }
+
 }
